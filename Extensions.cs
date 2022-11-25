3bcc110e9244cab7ee9f3fa6eec56b1755d0c844
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2022-11-26 05:32:13 +0800

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,24 @@
+﻿global using static GitUpdateAll.Extensions;
+
+namespace GitUpdateAll;
+
+public static class Extensions
+{
+    public static void LogError(string line = null) => Log(line, ConsoleColor.Red);
+    public static void Log(string line = null)
+    {
+        Console.WriteLine(line);
+    }
+    public static void Log(string line, ConsoleColor color)
+    {
+        var foregroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.WriteLine(line);
+        Console.ForegroundColor = foregroundColor;
+    }
+    public static PipeTarget 
+        LogPipe = PipeTarget.ToDelegate(Log),
+        LogErrorPipe = PipeTarget.ToDelegate(LogError);
+    public static CommandTask<CommandResult> ExecuteAsync(this Command @this, params string[] args) => @this.WithArguments(args).ExecuteAsync();
+    public static bool IsOk(this CommandResult @this) => @this.ExitCode == 0;
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿namespace GitUpdateAll;
+
+public class Program
+{
+    private Program() { }
+    public static readonly Program Instance = new();
+    public static int Main(string[] args) => Instance.InstanceMain(args);
+    private int InstanceMain(string[] args)
+    {
+        var task = InstanceAsyncMain(args);
+        task.Wait();
+        return task.Result;
+    }
+    private async Task<int> InstanceAsyncMain(string[] args)
+    {
+        var root = args.ElementAtOrDefault(0);
+        if (string.IsNullOrWhiteSpace(root))
+#if DEBUG
+            root = @"D:\repos\";
+#else
+            root = Environment.CurrentDirectory;
+#endif
+        var hasErrors = false;
+        var git = Cli.Wrap("git").WithValidation(CommandResultValidation.None);
+        var directories = Directory.GetDirectories(root, "", SearchOption.TopDirectoryOnly);
+        foreach (var directory in directories)
+        {
+            var localGit = git.WithWorkingDirectory(directory);
+            if ((await localGit.ExecuteAsync("status")).ExitCode != 0)
+                continue;
+            Log(Path.GetFileName(directory) + Environment.NewLine); // current directory
+            localGit = localGit.WithStandardErrorPipe(LogErrorPipe);
+            if (await TryFetchAndPull(localGit))
+                continue;
+            else Log();
+
+            hasErrors = true;
+        }
+        Log("Completed.", ConsoleColor.Cyan);
+        Console.ReadKey();
+        return hasErrors ? -1 : 0;
+    }
+
+    private static async Task<bool> TryFetchAndPull(Command git)
+    {
+        return (await git.ExecuteAsync("fetch")).IsOk() &&
+                (await git.ExecuteAsync("pull")).IsOk();
+    }
+}
